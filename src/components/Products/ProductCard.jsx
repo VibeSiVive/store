9d6ce8ef387da1f229e3b69109b984c8a50cb1a4
VibeSiVive <vbbataga2@JF610B-030>
2024-10-22 19:22:28 +0800

    router

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
+import React from "react";
 import Button from "../Button";
+import { Link } from "react-router-dom";
 
-const ProductCard = ({imageSrc, name}) => {
+const ProductCard = ({ imageSrc, name, id }) => {
   return (
-    <div className='border shadow-mdflex flex-col items-center gap-2 w-40 px-4 py-2'>
-    <img  src={imageSrc} className='w-24 h-[124px]' />
-    <span className='font-bold text-sm'>{name}</span>
+    <div className="border shadow-md flex flex-col items-center gap-2 w-40 px-4 py-2">
+      <img src={imageSrc} className="w-24 h-[124px]" />
+      <span className="font-bold text-sm">{name}</span>
 
-    <div>
-      <Button 
-      name="View" onClick={() => console.log(name)} 
-      className="bg-green-500 hover:bg-green-600/70" />
-        </div>
+      <div>
+        <Link
+          to={`/${id}`}
+          className="bg-green-500 hover:bg-green-500/70 px-4 py-2 rounded-lg"
+        >
+          View
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
